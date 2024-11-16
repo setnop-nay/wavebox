6601eafc18ae36018e9885e2b4aea7f18b774401
nayla <nayla@setnop>
2024-11-16 16:33:10 -0300

    criando do 0.

---
+++ Wavebox/src/main/java/com/wavebox/WaveboxApplication.java
@@ -0,0 +1,13 @@
+package com.wavebox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveboxApplication.class, args);
+	}
+
+}
---
+++ Wavebox/src/test/java/com/wavebox/WaveboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wavebox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
